215d4d7315e45d098ef1f48719f71d54a1dcd087
Sumi Sastri <sumisastri@Sumis-MacBook-Pro.local>
2019-08-20 14:09:29 +0100

    styling-form-input

--- src/components/form-input-fields/input.js
+++ src/components/form-input-fields/input.js
@@ -1,20 +1,92 @@
 import React from 'react';
 import './input.css';
+import FormHeader from '../form-navbars/formHeader';
+import FormFooter from '../form-navbars/formFooter';
 
-const Input = (properties) => {
-	return (
-        <form name="input-fields">
-        <div>
-            <label>Input-Field-One</label>
-            <input
-                name="input-field-one"
-                type="text"
-                placeholder="input-field-one"
-                className="input-field-one"
-            />
-        </div>
-        <br></br>
-        </form>
-	);
+const Input = properties => {
+  return (
+    <form name='input-fields'>
+      <FormHeader />
+      <div>
+        <label>Text-Input-One</label>
+        <input
+          name='input-field-one'
+          type='text'
+          placeholder='full-screen-field'
+          className='input-field-one'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Text-Input-Two</label>
+        <input
+          name='input-field-two'
+          type='text'
+          placeholder='field-with-colored-background'
+          className='input-field-two'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Text-Input-Three</label>
+        <input
+          name=''
+          type='text'
+          placeholder='half-screen-width'
+          className='input-field-three'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Text-Input-Four</label>
+        <input
+          name='input-field-four'
+          type='text'
+          placeholder='quarter-screen-width'
+          className='input-field-four'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Password Field</label>
+        <input
+          name='password-field-1'
+          type='password'
+          placeholder='password between 6 to 12 characters'
+          className='password-field-1'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Password Field</label>
+        <input
+          name='password-field-2'
+          type='password'
+          placeholder='password between 6 to 12 characters'
+          className='password-field-2'
+        />
+      </div>
+      <br />
+      <div>
+        <label>Numbers-Field (Rectangle)</label>
+        <input
+          name='Input-Numbers-1'
+          type='number'
+          placeholder=''
+          className='numbers-1'
+        />
+      </div>
+      <div>
+        <label>Numbers-Field (Pill)</label>
+        <input
+          name='Input-Numbers-2'
+          type='number'
+          placeholder=''
+          className='numbers-2'
+        />
+      </div>
+      <FormFooter />
+    </form>
+  );
 };
 export default Input;
--- src/components/mainForm.js
+++ src/components/mainForm.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import FormHeader from './form-navbars/formHeader';
 import Input from './form-input-fields/input';
-import RadioButtons from './form-buttons/radioButtons';
-import Button from './form-buttons/button';
-import FormFooter from './form-navbars/formFooter';
-import SelectFilter from './form-select-filters/filter';
-import CheckBox from './check-boxes/checkbox'
 
 class MainForm extends Component {
   render() {
     return (
       <div className='main-form-container'>
-        <FormHeader />
         <Input />
-        <CheckBox />
-        <SelectFilter />
-        <Button />
-        <br />
-        <br />
-        <RadioButtons />
-        <hr />
-        <FormFooter />
+            
       </div>
     );
   }
